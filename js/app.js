1bfebe4f7577b55e64fb111de07baa5c3e3106a6
Michael Miller <pfxmichaelm@gmail.com>
2015-02-06 09:23:55 -0700

    second round of editing

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		console.log('mousedown');
+		// play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function() {
+		console.log('mouseup');
+		//ryu goes back to his ready position
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+});
